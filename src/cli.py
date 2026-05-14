b030a2b0be3a0dbda5b7735aacba63c04b10cea6
SergeyK1983 <ssp-serg@yandex.ru>
2026-05-14 22:20:13 +0300

    feat(smp-1) Added command createsuperuser

---
+++ src/cli.py
@@ -0,0 +1,28 @@
+import asyncio
+
+import typer
+from rich import print
+
+from src.auth.commands.create_superuser import create_superuser
+
+
+app = typer.Typer()
+
+
+@app.command()
+def createsuperuser(
+        username: str = typer.Argument(..., help="Имя пользователя"),
+        pwd: str = typer.Argument(..., help="Пароль")
+) -> None:
+    msg: str = asyncio.run(create_superuser(username, pwd))
+    print(f"[bold green]{msg}[/bold green]")
+    return
+
+
+@app.command()
+def hello():
+    print(f"[bold green]Приветствие от Small project![/bold green]")
+
+
+if __name__ == '__main__':
+    app()
