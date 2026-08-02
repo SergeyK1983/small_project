59eb1095e8879f382a23ad884c1430d8c33c552c
SergeyK1983 <ssp-serg@yandex.ru>
2026-08-02 15:32:58 +0300

    test(smp-4) Added test_change_user_pwd

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,12 +1,19 @@
-from fastapi.testclient import TestClient
-import pytest_asyncio
+from unittest.mock import patch
+from uuid import uuid4
+from typing import TYPE_CHECKING, Any, AsyncGenerator, Generator
+
 import pytest
-from typing import TYPE_CHECKING, Any, AsyncGenerator
+import pytest_asyncio
+from fastapi.datastructures import State
+from fastapi.testclient import TestClient
 from httpx import ASGITransport, AsyncClient
 
+from src.auth.schemas.output.user_base import UserBase
 from src.main import app
 from src.core.database import db_helper
+
 from .factories import UserFactory
+
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -28,6 +35,19 @@ async def ac() -> AsyncGenerator[AsyncClient, Any]:
 def client() -> TestClient:
     client = TestClient(app, base_url="http://localhost")
     return client
+
+
+@pytest.fixture
+def client_with_user(monkeypatch) -> Generator[tuple[TestClient, UserFactory]]:
+    uf = UserFactory.build(id=uuid4(), username="test_user")
+    user = UserBase.model_validate(uf, from_attributes=True)
+    
+    with patch("src.middleware.auth_middleware.authenticate_middleware") as mock_auth:
+        mock_auth.return_value = True
+        monkeypatch.setattr(State, "user", user, raising=False)
+
+        client = TestClient(app, base_url="http://localhost", headers={"Authorization": "JWT tratata"})
+        yield client, uf
 
 
 @pytest_asyncio.fixture
